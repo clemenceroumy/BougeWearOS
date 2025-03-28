38831e6e42613eba1f1a1fc2c31395a5d8bda222
clemence roumy <clemenceroumy@gmail.com>
2025-03-28 12:57:06 +0100

    init project

---
+++ wear/src/main/java/fr/croumy/bouge/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package fr.croumy.bouge.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun BougeTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
