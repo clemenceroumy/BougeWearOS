2ad96e7e15dab3d15e6db3fcc9466c03cd9e378d
clemenceroumy <clemenceroumy@gmail.com>
2025-08-07 17:15:07 +0200

    feat: custom btn component

---
+++ wear/src/main/java/fr/croumy/bouge/presentation/theme/Dimensions.kt
@@ -0,0 +1,11 @@
+package fr.croumy.bouge.presentation.theme
+
+import androidx.compose.ui.unit.dp
+
+object Dimensions {
+    val mediumPadding = 16.dp
+
+    val mediumRadius = 16.dp
+
+    val btnHeight = 48.dp
+}
--- wear/src/main/java/fr/croumy/bouge/presentation/ui/screens/redirect/RedirectScreen.kt
+++ wear/src/main/java/fr/croumy/bouge/presentation/ui/screens/redirect/RedirectScreen.kt
@@ -3,15 +3,15 @@ package fr.croumy.bouge.presentation.ui.screens.redirect
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
-import androidx.wear.compose.material.Button
 import com.google.android.horologist.annotations.ExperimentalHorologistApi
 import fr.croumy.bouge.presentation.injection.LocalNavController
 import fr.croumy.bouge.presentation.navigation.NavRoutes
+import fr.croumy.bouge.presentation.theme.Dimensions
+import fr.croumy.bouge.presentation.ui.components.Button
 
 @OptIn(ExperimentalHorologistApi::class)
 @Composable
@@ -21,15 +21,14 @@ fun RedirectScreen() {
     ScalingLazyColumn(
         Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(Dimensions.mediumPadding)
     ) {
         item {
             Button(
                 modifier = Modifier.fillMaxWidth(),
-                onClick = { navController.navigate(NavRoutes.ExercisesHistory.route) }
-            ) {
-                Text("Walks")
-            }
+                onClick = { navController.navigate(NavRoutes.ExercisesHistory.route) },
+                label = "walks"
+            )
         }
     }
 }
