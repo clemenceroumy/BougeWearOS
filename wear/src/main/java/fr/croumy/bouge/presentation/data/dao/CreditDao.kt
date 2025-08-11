95a3339130c7e716a1bdb1093b94b9688660ae8f
Clémence Roumy <clemenceroumy@gmail.com>
2025-08-11 18:55:08 +0200

    fix: credits db table

--- wear/src/main/java/fr/croumy/bouge/presentation/data/dao/CreditDao.kt
+++ wear/src/main/java/fr/croumy/bouge/presentation/data/dao/CreditDao.kt
@@ -1,9 +1,11 @@
 package fr.croumy.bouge.presentation.data.dao
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import fr.croumy.bouge.presentation.data.entities.CreditEntity
 
+@Dao
 interface CreditDao {
     @Query("SELECT SUM(value) FROM credits")
     fun getTotal(): Int
--- wear/src/main/java/fr/croumy/bouge/presentation/data/entities/CreditEntity.kt
+++ wear/src/main/java/fr/croumy/bouge/presentation/data/entities/CreditEntity.kt
@@ -1,10 +1,14 @@
 package fr.croumy.bouge.presentation.data.entities
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import fr.croumy.bouge.presentation.models.ExerciseType
+import java.time.ZonedDateTime
 
 @Entity(tableName = "credits")
 data class CreditEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int,
     val value: Int,
-    val type: ExerciseType
+    val type: ExerciseType,
+    val timestamp: ZonedDateTime = ZonedDateTime.now()
 )
