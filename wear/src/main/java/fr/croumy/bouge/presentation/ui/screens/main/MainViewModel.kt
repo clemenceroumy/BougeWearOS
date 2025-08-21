9d4179a3455546b74a350ba7aa1444d14eb6c555
Clémence Roumy <clemenceroumy@gmail.com>
2025-08-21 11:14:49 +0200

    refactor: remove unused import

--- wear/src/main/java/fr/croumy/bouge/presentation/ui/screens/main/MainViewModel.kt
+++ wear/src/main/java/fr/croumy/bouge/presentation/ui/screens/main/MainViewModel.kt
@@ -1,10 +1,7 @@
 package fr.croumy.bouge.presentation.ui.screens.main
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import fr.croumy.bouge.presentation.services.HealthService
-import fr.croumy.bouge.presentation.repositories.SensorRepository
 import fr.croumy.bouge.presentation.services.DataService
 import javax.inject.Inject
 
