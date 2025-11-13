f03d5600882829c799d33b96002e5d638b84805e
clemenceroumy <clemenceroumy@gmail.com>
2025-11-13 10:38:53 +0100

    comment

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     // can't use atm because moko-resources not up to date -> https://github.com/icerockdev/moko-resources/issues/843
     //alias(libs.plugins.android.kotlinMultiplatform.library)
+    // using deprecated com.android.library until moko-resources is fixed
     id("com.android.library")
 
     id("dev.icerock.mobile.multiplatform-resources")
@@ -17,6 +18,7 @@ android {
 
 kotlin {
     androidTarget()
+    // uncomment when using libs.plugins.android.kotlinMultiplatform.library
     /*androidLibrary {
         namespace = "fr.croumy.bouge.core"
         compileSdk = 36
