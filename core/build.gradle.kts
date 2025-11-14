9e53cf75445421c60758ef5278d50eeb601b1ded
clemenceroumy <clemenceroumy@gmail.com>
2025-11-14 12:27:23 +0100

    refactor: remove unused lib (moko-resources)

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,41 +3,25 @@ plugins {
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.composeHotReload)
-
-    // can't use atm because moko-resources not up to date -> https://github.com/icerockdev/moko-resources/issues/843
-    //alias(libs.plugins.android.kotlinMultiplatform.library)
-    // using deprecated com.android.library until moko-resources is fixed
-    id("com.android.library")
-
-    id("dev.icerock.mobile.multiplatform-resources")
+    alias(libs.plugins.android.kotlinMultiplatform.library)
     id("kotlinx-serialization")
 }
 
-android {
-    namespace = "fr.croumy.bouge.core"
-    compileSdk = 36
-    defaultConfig {
-        minSdk = 24
-    }
-}
-
 kotlin {
-    androidTarget()
-    // uncomment when using libs.plugins.android.kotlinMultiplatform.library
-    /*androidLibrary {
+    androidLibrary {
         namespace = "fr.croumy.bouge.core"
         compileSdk = 36
         minSdk = 24
 
         withJava()
         androidResources.enable = true
-    }*/
+    }
+
     jvm("core")
 
     sourceSets {
         commonMain {
             dependencies {
-                implementation("dev.icerock.moko:resources:0.25.1")
                 implementation(libs.kotlinx.serialization.json)
 
                 implementation(compose.runtime)
@@ -51,9 +35,4 @@ kotlin {
             }
         }
     }
-}
-
-multiplatformResources {
-    resourcesPackage.set("fr.croumy.bouge.core.mr")
-    resourcesClassName.set("SharedRes") //default "MR"
 }
--- core/src/androidMain/kotlin/fr/croumy/bouge/core/Platform.android.kt
+++
@@ -1,1 +0,0 @@
-package fr.croumy.bouge.core
