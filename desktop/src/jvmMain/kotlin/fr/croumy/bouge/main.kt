409ea336bec95129731537562eed37850abfb311
Clémence Roumy <clemenceroumy@gmail.com>
2025-11-25 15:05:38 +0100

    feat: wip: companion walks on screen

--- desktop/src/jvmMain/kotlin/fr/croumy/bouge/main.kt
+++ desktop/src/jvmMain/kotlin/fr/croumy/bouge/main.kt
@@ -5,63 +5,72 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalWindowInfo
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Tray
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
+import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.WindowSize
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberTrayState
+import androidx.compose.ui.window.rememberWindowState
 import fr.croumy.bouge.services.BleScanner
 import fr.croumy.bouge.ui.MainScreen
 
 fun main() = application {
     val companion = BleScanner.currentCompanion
+    val trayState = rememberTrayState()
+    val windowState = rememberWindowState(
+        placement = WindowPlacement.Floating,
+        position = WindowPosition.Aligned(Alignment.BottomEnd),
+        size = DpSize(500.dp, 100.dp)
+    )
+
+    val isConnected = BleScanner.isConnected.collectAsState()
+    val peripherals = BleScanner.peripherals
+
+    Tray(
+        icon = TrayIcon,
+        state = trayState,
+        tooltip = "",
+        onAction = {},
+        menu = {
+            Item(
+                "Connect",
+                onClick = { BleScanner.scan() },
+                enabled = !BleScanner.isScanning.value && !isConnected.value
+            )
+            peripherals.value.map {
+                Item(
+                    it.identifier.toString(),
+                    onClick = { BleScanner.selectPeripheral(it) }
+                )
+            }
+
+            Item(
+                "Exit",
+                onClick = ::exitApplication
+            )
+        }
+    )
 
     Window(
         onCloseRequest = ::exitApplication,
         title = "BougeWearOS",
+        state = windowState,
         transparent = true,
-        undecorated = true
+        undecorated = true,
+        alwaysOnTop = true,
+        resizable = false
         //visible = isVisible.value
     ) {
-        val state = rememberTrayState()
-
-        val isConnected = BleScanner.isConnected.collectAsState()
-        val peripherals = BleScanner.peripherals
-
-        LaunchedEffect(BleScanner.isScanning.value) {
-            println(BleScanner.isScanning.value)
-        }
-
-        Column {
-            Tray(
-                icon = TrayIcon,
-                state = state,
-                tooltip = "",
-                onAction = {},
-                menu = {
-                    Item(
-                        "Connect",
-                        onClick = { BleScanner.scan() },
-                        enabled = !BleScanner.isScanning.value && !isConnected.value
-                    )
-                    peripherals.value.map {
-                        Item(
-                            it.toString(),
-                            onClick = { BleScanner.selectPeripheral(it) }
-                        )
-                    }
-
-                    Item(
-                        "Exit",
-                        onClick = ::exitApplication
-                    )
-                }
-            )
-        }
-
         if (companion.value != null) {
             MainScreen(companion.value!!)
         }
--- desktop/src/jvmMain/kotlin/fr/croumy/bouge/ui/MainScreen.kt
+++ desktop/src/jvmMain/kotlin/fr/croumy/bouge/ui/MainScreen.kt
@@ -1,23 +1,56 @@
 package fr.croumy.bouge.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
 import fr.croumy.bouge.core.models.companion.Companion
 import fr.croumy.bouge.core.ui.components.AnimatedSprite
+import fr.croumy.bouge.models.Direction
+import kotlinx.coroutines.delay
 
 @Composable
 fun MainScreen(
     companion: Companion
 ) {
-    Column {
-        Text(companion.name)
-        Text(companion.age.toString())
+    val direction = mutableStateOf(Direction.RIGHT)
+    val moveValue = remember { mutableStateOf(0f) }
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            delay(800L) // ANIMATION SPEED
+            direction.value = Direction.random()
+            println(direction.value)
+            moveValue.value = when(direction.value){
+                Direction.LEFT -> moveValue.value - 10f
+                Direction.RIGHT -> moveValue.value + 10f
+            }
+        }
+    }
+
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         AnimatedSprite(
-            modifier = Modifier.size(120.dp),
+            modifier = Modifier
+                .size(70.dp)
+                .graphicsLayer {
+                    rotationY = when(direction.value){
+                        Direction.LEFT -> 180f
+                        Direction.RIGHT -> 0f
+                    }
+                    translationX = moveValue.value
+                },
             imageId = companion.type.assetIdleId,
             frameCount = companion.type.assetIdleFrame,
         )
