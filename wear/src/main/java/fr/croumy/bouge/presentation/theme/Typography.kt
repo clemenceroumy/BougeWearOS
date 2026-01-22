984007fa289ce9ce4f995d89509e3f6bad95efba
Clémence Roumy <clemenceroumy@gmail.com>
2026-01-22 17:01:18 +0100

    feat: import font

--- wear/src/main/java/fr/croumy/bouge/presentation/theme/Typography.kt
+++ wear/src/main/java/fr/croumy/bouge/presentation/theme/Typography.kt
@@ -2,17 +2,29 @@ package fr.croumy.bouge.presentation.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.sp
+import fr.croumy.bouge.R
 
-
+val jerseyFont = FontFamily(
+    Font(resId = R.font.jersey15)
+)
 fun AppTypography() = Typography(
+    bodyLarge = TextStyle(
+        fontSize = 18.sp,
+        fontFamily = jerseyFont
+    ),
     bodyMedium = TextStyle(
-        fontSize = 12.sp
+        fontSize = 16.sp,
+        fontFamily = jerseyFont
     ),
     bodySmall = TextStyle(
-        fontSize = 10.sp
+        fontSize = 14.sp,
+        fontFamily = jerseyFont
     ),
     labelMedium = TextStyle(
-        fontSize = 8.sp
+        fontSize = 10.sp,
+        fontFamily = jerseyFont
     )
 )
