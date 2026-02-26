4b9c50799a2e36659c675d0aae278d891e347f64
Clémence Roumy <clemenceroumy@gmail.com>
2026-02-26 11:21:37 +0100

    feat: empty text and total deads ui

--- wear/src/main/java/fr/croumy/bouge/presentation/ui/screens/historyCompanion/HistoryCompanionScreen.kt
+++ wear/src/main/java/fr/croumy/bouge/presentation/ui/screens/historyCompanion/HistoryCompanionScreen.kt
@@ -2,8 +2,10 @@ package fr.croumy.bouge.presentation.ui.screens.historyCompanion
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,19 +21,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
+import androidx.wear.compose.foundation.lazy.ScalingLazyListAnchorType
 import androidx.wear.compose.foundation.lazy.items
+import com.google.android.horologist.compose.layout.fillMaxRectangle
 import fr.croumy.bouge.R
 import fr.croumy.bouge.presentation.extensions.asString
 import fr.croumy.bouge.presentation.theme.Dimensions
 import fr.croumy.bouge.core.ui.components.AnimatedSprite
+import fr.croumy.bouge.presentation.extensions.fillMaxRectangleWidth
 import fr.croumy.bouge.presentation.extensions.toYYYYMMDD
 import fr.croumy.bouge.presentation.ui.components.NinePatchImage
+import fr.croumy.bouge.presentation.ui.components.OutlinedText
 
 @Composable
 fun HistoryCompanionScreen(
@@ -46,68 +54,111 @@ fun HistoryCompanionScreen(
             contentDescription = "",
         )
 
-        ScalingLazyColumn {
-            items(viewModel.companions.value) { companion ->
+        if (viewModel.companions.value.isEmpty()) {
+            Column(
+                modifier = Modifier.fillMaxRectangle(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painterResource(R.drawable.icon_death),
+                    contentDescription = null,
+                    modifier = Modifier.size(Dimensions.smallIcon)
+                )
+                Spacer(Modifier.height(Dimensions.smallPadding))
+                OutlinedText(
+                    text = stringResource(R.string.companions_dead_empty),
+                    style = MaterialTheme.typography.bodyMedium,
+                    textAlign = TextAlign.Center
+                )
+            }
+        } else {
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .padding(top = Dimensions.smallPadding),
+            ) {
                 Box(
-                    Modifier.fillMaxWidth()
+                    modifier = Modifier.fillMaxRectangleWidth(),
+                    contentAlignment = Alignment.Center
                 ) {
-                    NinePatchImage(
-                        modifier = Modifier.matchParentSize(),
-                        resId = R.drawable.cloud_btn,
+                    OutlinedText(
+                        text = stringResource(R.string.companions_dead_total, viewModel.companions.value.size),
+                        style = MaterialTheme.typography.labelSmall,
+                        textAlign = TextAlign.Center
                     )
+                }
 
-                    Row(
-                        Modifier
-                            .fillMaxWidth()
-                            .padding(Dimensions.smallPadding),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
+                ScalingLazyColumn(
+                    contentPadding = PaddingValues(
+                        vertical = Dimensions.smallPadding,
+                        horizontal = Dimensions.mediumPadding
+                    ),
+                    anchorType = ScalingLazyListAnchorType.ItemStart
+                ) {
+                    items(viewModel.companions.value) { companion ->
                         Box(
-                            modifier = Modifier.padding(end = Dimensions.smallIcon / 2 + Dimensions.smallPadding)
+                            Modifier.fillMaxWidth()
                         ) {
-                            Image(
-                                modifier = Modifier
-                                    .size(Dimensions.smallIcon)
-                                    .offset(x = Dimensions.smallIcon / 2),
-                                painter = painterResource(id = R.drawable.grave),
-                                contentDescription = ""
+                            NinePatchImage(
+                                modifier = Modifier.matchParentSize(),
+                                resId = R.drawable.cloud_btn,
                             )
-                            AnimatedSprite(
-                                modifier = Modifier
-                                    .size(Dimensions.smallIcon),
-                                imageId = companion.type.assetIdleId,
-                                frameCount = companion.type.assetIdleFrame,
-                            )
-                        }
-                        Column {
-                            Text(companion.name, style = MaterialTheme.typography.bodyLarge)
-                            Text(
-                                buildAnnotatedString {
-                                    withStyle(style = MaterialTheme.typography.displaySmall.toSpanStyle()) {
-                                        append(companion.age.toString())
-                                    }
-                                    withStyle(style = MaterialTheme.typography.bodySmall.toSpanStyle()) {
-                                        append(stringResource(R.string.companion_days_old))
-                                    }
-                                }
-                            )
+
                             Row(
+                                Modifier
+                                    .fillMaxWidth()
+                                    .padding(Dimensions.smallPadding),
                                 verticalAlignment = Alignment.CenterVertically
                             ) {
-                                Image(
-                                    painterResource(R.drawable.icon_birth),
-                                    contentDescription = "",
-                                    modifier = Modifier
-                                        .size(Dimensions.xxxsmallIcon)
-                                )
-                                Text(companion.birthDate.toYYYYMMDD(), style = MaterialTheme.typography.labelMedium)
-                                Spacer(Modifier.width(Dimensions.xxsmallPadding))
-                                Image(
-                                    painterResource(R.drawable.icon_death),
-                                    contentDescription = "",
-                                    modifier = Modifier.size(Dimensions.xxxsmallIcon)
-                                )
-                                Text(companion.deathDate!!.toYYYYMMDD(), style = MaterialTheme.typography.labelMedium)
+                                Box(
+                                    modifier = Modifier.padding(end = Dimensions.smallIcon / 2 + Dimensions.smallPadding)
+                                ) {
+                                    Image(
+                                        modifier = Modifier
+                                            .size(Dimensions.smallIcon)
+                                            .offset(x = Dimensions.smallIcon / 2),
+                                        painter = painterResource(id = R.drawable.grave),
+                                        contentDescription = ""
+                                    )
+                                    AnimatedSprite(
+                                        modifier = Modifier
+                                            .size(Dimensions.smallIcon),
+                                        imageId = companion.type.assetIdleId,
+                                        frameCount = companion.type.assetIdleFrame,
+                                    )
+                                }
+                                Column {
+                                    Text(companion.name, style = MaterialTheme.typography.bodyLarge)
+                                    Text(
+                                        buildAnnotatedString {
+                                            withStyle(style = MaterialTheme.typography.displaySmall.toSpanStyle()) {
+                                                append(companion.age.toString())
+                                            }
+                                            withStyle(style = MaterialTheme.typography.bodySmall.toSpanStyle()) {
+                                                append(stringResource(R.string.companion_days_old))
+                                            }
+                                        }
+                                    )
+                                    Row(
+                                        verticalAlignment = Alignment.CenterVertically
+                                    ) {
+                                        Image(
+                                            painterResource(R.drawable.icon_birth),
+                                            contentDescription = "",
+                                            modifier = Modifier
+                                                .size(Dimensions.xxxsmallIcon)
+                                        )
+                                        Text(companion.birthDate.toYYYYMMDD(), style = MaterialTheme.typography.labelMedium)
+                                        Spacer(Modifier.width(Dimensions.xxsmallPadding))
+                                        Image(
+                                            painterResource(R.drawable.icon_death),
+                                            contentDescription = "",
+                                            modifier = Modifier.size(Dimensions.xxxsmallIcon)
+                                        )
+                                        Text(companion.deathDate!!.toYYYYMMDD(), style = MaterialTheme.typography.labelMedium)
+                                    }
+                                }
                             }
                         }
                     }
