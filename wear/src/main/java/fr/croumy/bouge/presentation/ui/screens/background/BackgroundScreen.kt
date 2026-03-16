3cd17b7193d06de5c4eb839bb98b4357d78c0d6d
Clémence Roumy <clemenceroumy@gmail.com>
2026-03-16 14:44:48 +0100

    feat: ui backgrounds selection

--- wear/src/main/java/fr/croumy/bouge/presentation/ui/screens/background/BackgroundScreen.kt
+++ wear/src/main/java/fr/croumy/bouge/presentation/ui/screens/background/BackgroundScreen.kt
@@ -5,9 +5,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -19,12 +22,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.android.horologist.compose.layout.fillMaxRectangle
+import fr.croumy.bouge.R
+import fr.croumy.bouge.presentation.models.app.ShopItemType
 import fr.croumy.bouge.presentation.theme.Dimensions
+import fr.croumy.bouge.presentation.ui.components.ShopItemComponent
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
@@ -32,41 +39,51 @@ import org.jetbrains.compose.resources.stringResource
 fun BackgroundScreen(
     viewModel: BackgroundViewModel = hiltViewModel()
 ) {
-    LazyVerticalGrid(
-        modifier = Modifier.fillMaxRectangle(),
-        columns = GridCells.Fixed(2),
-        verticalArrangement = Arrangement.spacedBy(Dimensions.xsmallPadding),
-        horizontalArrangement = Arrangement.spacedBy(Dimensions.xsmallPadding)
-    ) {
-        items(viewModel.allBackgroundItems.value) { item ->
-            val isSelected = viewModel.companion.value?.backgroundId == item.id
+    Box {
+        Image(
+            painterResource(R.drawable.background_sky_day),
+            contentDescription = stringResource(R.string.description_cloudy_background),
+        )
 
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(1f)
-                    .background(MaterialTheme.colorScheme.surface, RoundedCornerShape(Dimensions.mediumRadius))
-                    .border(
-                        1.dp,
-                        if (isSelected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
-                        RoundedCornerShape(Dimensions.mediumRadius)
+        LazyVerticalGrid(
+            modifier = Modifier.fillMaxRectangle(),
+            columns = GridCells.Fixed(2),
+            verticalArrangement = Arrangement.spacedBy(Dimensions.xsmallPadding),
+            horizontalArrangement = Arrangement.spacedBy(Dimensions.xsmallPadding)
+        ) {
+            items(viewModel.allBackgroundItems.value) { item ->
+                val isSelected = viewModel.companion.value?.backgroundId == item.id
+
+                Box(
+                    Modifier
+                        .fillMaxSize()
+                        .clickable{ viewModel.selectBackground(item.id) }
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.inventory_box),
+                        contentDescription = null,
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.Crop
                     )
-                    .clickable { viewModel.selectBackground(item.id) }
-                    .padding(Dimensions.xsmallPadding),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(
-                    stringResource(item.name),
-                    style = MaterialTheme.typography.bodySmall
-                )
-                Image(
-                    painter = painterResource(item.assetId),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(Dimensions.iconBtnHeight)
-                        .aspectRatio(1f)
-                )
+                    Image(
+                        painter = painterResource(item.assetId),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(Dimensions.smallPadding + Dimensions.xsmallPadding)
+                            .offset(y = -Dimensions.smallPadding)
+                    )
+
+                    if(isSelected) {
+                        Image(
+                            painterResource(R.drawable.icon_selected),
+                            contentDescription = stringResource(R.string.description_selected_wallpaper),
+                            modifier = Modifier
+                                .size(Dimensions.xsmallIcon)
+                                .align(Alignment.TopEnd)
+                        )
+                    }
+                }
             }
         }
     }
