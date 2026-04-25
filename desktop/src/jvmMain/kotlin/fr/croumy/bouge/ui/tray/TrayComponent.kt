784abdc0b771d96605a1cabb794996c4f7e9b920
clemenceroumy <clemenceroumy@gmail.com>
2026-04-25 11:48:18 +0200

    refactor: upgrade all libs (and add composenativetray lib)

--- desktop/src/jvmMain/kotlin/fr/croumy/bouge/ui/tray/TrayComponent.kt
+++ desktop/src/jvmMain/kotlin/fr/croumy/bouge/ui/tray/TrayComponent.kt
@@ -1,11 +1,14 @@
 package fr.croumy.bouge.ui.tray
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ApplicationScope
 import androidx.compose.ui.window.Tray
 import androidx.compose.ui.window.rememberTrayState
@@ -13,6 +16,7 @@ import bouge.desktop.generated.resources.Res
 import bouge.desktop.generated.resources.menu_connect
 import bouge.desktop.generated.resources.menu_disconnect
 import bouge.desktop.generated.resources.menu_exit
+
 import fr.croumy.bouge.services.BleScanner
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -30,31 +34,58 @@ fun ApplicationScope.TrayComponent(
 
     val coroutineScope = CoroutineScope(Dispatchers.IO)
 
+    val connectionLabel = stringResource(if (isConnected.value) Res.string.menu_disconnect else Res.string.menu_connect)
+    val exitLabel = stringResource(Res.string.menu_exit)
+
     Tray(
         icon = TrayIcon,
-        state = trayState,
         tooltip = "",
-        onAction = {},
-        menu = {
-            Item(
-                stringResource(if (isConnected.value) Res.string.menu_disconnect else Res.string.menu_connect),
-                onClick = {
-                    if (isConnected.value) coroutineScope.launch { bleScanner.disconnect() }
-                    else bleScanner.scan()
-                },
-                enabled = !bleScanner.isScanning.value
-            )
+    ) {
+        Item(
+            connectionLabel,
+            onClick = {
+                if (isConnected.value) coroutineScope.launch { bleScanner.disconnect() }
+                else bleScanner.scan()
+            },
+            //enabled = !bleScanner.isScanning.value
+        )
 
-            Item(
-                stringResource(Res.string.menu_exit),
-                onClick = {
-                    coroutineScope.launch {
-                        exitApplication()
-                    }
+        Item(
+            exitLabel,
+            onClick = {
+                coroutineScope.launch {
+                    exitApplication()
                 }
-            )
-        }
+            }
+        )
+    }
+
+    /*val trayAppState = rememberTrayAppState(
+        initialWindowSize = DpSize(300.dp, 420.dp),
+        initiallyVisible = true // default is false
+        // initialDismissMode defaults to TrayWindowDismissMode.AUTO
     )
+
+    TrayApp(
+        state = trayAppState,
+        icon = TrayIcon,
+        tooltip = "My Tray App",          // required
+
+        // Optional visual controls (defaults shown below)
+        transparent = true,               // default = true
+        undecorated = true,               // default = true
+        resizable = false,                // default = false
+        windowsTitle = "My Tray Popup",   // default = "" — recommended (esp. on Linux & when undecorated=false)
+        windowIcon = null,                // default = null — set your app icon; important on Linux & when undecorated=false
+
+        menu = {                          // optional (default = null)
+            Item("Toggle popup") { trayAppState.toggle() }
+            Divider()
+            Item("Quit") { exitApplication() }
+        }
+    ) {
+            Text("Quick Settings")
+    }*/
 }
 
 object TrayIcon : Painter() {
--- wear/src/main/java/fr/croumy/bouge/presentation/ui/screens/background/BackgroundScreen.kt
+++ wear/src/main/java/fr/croumy/bouge/presentation/ui/screens/background/BackgroundScreen.kt
@@ -1,15 +1,12 @@
 package fr.croumy.bouge.presentation.ui.screens.background
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -17,34 +14,38 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.input.rotary.onRotaryScrollEvent
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.android.horologist.annotations.ExperimentalHorologistApi
 import com.google.android.horologist.compose.layout.fillMaxRectangle
-import com.google.android.horologist.compose.rotaryinput.rotaryWithScroll
 import fr.croumy.bouge.R
-import fr.croumy.bouge.presentation.models.app.ShopItemType
 import fr.croumy.bouge.presentation.theme.Dimensions
-import fr.croumy.bouge.presentation.ui.components.ShopItemComponent
+import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.resources.stringResource
 
 @OptIn(ExperimentalHorologistApi::class)
 @Composable
 fun BackgroundScreen(
     viewModel: BackgroundViewModel = hiltViewModel()
 ) {
+    val coroutineScope = rememberCoroutineScope()
     val lazyGridState = rememberLazyGridState()
+    val focusRequester = remember { FocusRequester() }
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
 
     Box {
         Image(
@@ -56,7 +57,14 @@ fun BackgroundScreen(
             state = lazyGridState,
             modifier = Modifier
                 .fillMaxRectangle()
-                .rotaryWithScroll(scrollableState = lazyGridState),
+                .onRotaryScrollEvent { event ->
+                    coroutineScope.launch {
+                        lazyGridState.scrollBy(event.verticalScrollPixels)
+                    }
+                    true
+                }
+                .focusRequester(focusRequester)
+                .focusable(),
             columns = GridCells.Fixed(2),
             verticalArrangement = Arrangement.spacedBy(Dimensions.xsmallPadding),
             horizontalArrangement = Arrangement.spacedBy(Dimensions.xsmallPadding)
